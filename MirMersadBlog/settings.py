2507b2ae45829ac7377a28300eaa399f787a7bbd
mersad29 <mersad.mir96@gmail.com>
2025-03-01 00:12:45 +0330

    Deployment Test - 11:55 PM

--- MirMersadBlog/settings.py
+++ MirMersadBlog/settings.py
@@ -147,8 +147,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
