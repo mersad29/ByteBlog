b3c0cbf0a7021f604453fd0b10ab440103de1171
mersad29 <mersad.mir96@gmail.com>
2025-03-01 01:47:04 +0330

    Deployment Test - 11:55 PM

--- MirMersadBlog/settings.py
+++ MirMersadBlog/settings.py
@@ -150,10 +150,10 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-#
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 
