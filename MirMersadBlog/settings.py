e39e3d9302f35b619b508b77d36c6c932970cd43
mersad29 <mersad.mir96@gmail.com>
2025-03-01 00:24:14 +0330

    Deployment Test - 11:55 PM

--- MirMersadBlog/settings.py
+++ MirMersadBlog/settings.py
@@ -151,10 +151,9 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "/static/"
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
