6271464a8137eac299e9e1f1d15e73a9c10f5a7d
mersad29 <mersad.mir96@gmail.com>
2025-03-01 16:50:37 +0330

    Deployment Test - 1/3/2025 4:50 PM

--- MirMersadBlog/urls.py
+++ MirMersadBlog/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Index.urls')),
     path('articles/', include('Article.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
