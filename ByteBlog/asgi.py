f4ceee4257ea3176db0495847e1ed0729ea59190
mersad29 <mersad.mir96@gmail.com>
2025-07-01 02:28:27 +0330

    Change name to ByteBlog - 1/7/2025 2:28 AM

--- ByteBlog/asgi.py
+++ ByteBlog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MirMersadBlog project.
+ASGI config for ByteBlog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MirMersadBlog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ByteBlog.settings')
 
 application = get_asgi_application()
--- ByteBlog/wsgi.py
+++ ByteBlog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MirMersadBlog project.
+WSGI config for ByteBlog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MirMersadBlog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ByteBlog.settings')
 
 application = get_wsgi_application()
